3505758c89043306169f48f7eb8db0d2c9e46bdd
jesusd520 <jenarvaezd@unal.edu.co>
2025-05-20 16:54:59 -0500

    Fist commit

---
+++ src/Profesores/Prueba.java
@@ -0,0 +1,10 @@
+package Profesores;
+
+public class Prueba {
+
+    public static void main(String[] args) {
+        Profesor profesor1 = new ProfesorTitular();
+        profesor1.imprimir();
+    }
+    
+}
